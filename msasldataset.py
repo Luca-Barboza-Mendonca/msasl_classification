53885ae0b0ee72e9657699bb6952a2bed3697cb4
Luca-Barboza-Mendonca <118559271+Luca-Barboza-Mendonca@users.noreply.github.com>
2023-12-10 15:50:57 -0300

    Update msasldataset.py

--- msasldataset.py
+++ msasldataset.py
@@ -4,9 +4,11 @@ from torchvision import transforms
 from PIL import Image
 import pandas as pd
 import os
+import json
 
 class ImageSequenceDataset(Dataset):
-    def __init__(self, csv_file, transform=None):
+    def __init__(self, csv_file, folder, transform):
+        self.folder = folder
         self.annotations = pd.read_csv(csv_file)
         self.transform = transform
 
@@ -14,17 +16,31 @@ class ImageSequenceDataset(Dataset):
         return len(self.annotations)
 
     def __getitem__(self, idx):
-        img_sequence_path = self.annotations.iloc[idx, 0]
+        img_sequence_path = os.path.join(self.folder, self.annotations.iloc[idx, 0])
         images = []
         for i in range(0, 32):
             if i < 10:
                 images.append(Image.open(os.path.join(img_sequence_path, f"frame_000{i}.jpg")))
             else:
                 images.append(Image.open(os.path.join(img_sequence_path, f"frame_00{i}.jpg")))
-        # images = [Image.open(os.path.join(img_sequence_path, f"frame_00{i}.jpg")) for i in range(1, 33)]
         label = self.annotations.iloc[idx, 1]
 
-        if self.transform:
-            images = [self.transform(img) for img in images]
+        images = torch.stack([self.transform(img) for img in images], dim=0)
 
-        return torch.stack(images), label
+        return images.permute(1, 0, 2, 3), label
+
+def get_class_map(file_path):
+    file = open(file_path, 'r')
+
+    content = file.read()
+
+    class_list = json.loads(content)
+
+    class_list = class_list[:101]
+
+    class_map = {}
+
+    for i in range(0, len(class_list)):
+        class_map[class_list[i]] = i
+    
+    return class_map
